84f26dfaed5fead76b3c3cac1c47b691b279fa51
omgshokh <shohruz199905@gmail.com>
2025-02-08 21:55:42 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+// 1) alert(5%2)
+
+// 2) let number = Math.random() * 10;
+// number = Math.ceil(number)
+// alert(number)   
+
+// 3) let number = Math.floor(12.510);
+// alert(number)
+
+// 4) let user = 'MARS IT SCHOOL';
+// alert(user.length)
+
+// 5) for (i = 0; i < 10; i++)
+// console.log('MARS IT SCHOOL')
+
+// 6) let user = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z ']
+// console.log(user[18] + user[7] + user[14] + user[23] + user[17] + user[20] + user[25])
+
+// 7) let year = prompt('Yoshingizni kiriting?')
+// if(year < 18){
+//     message = ('siz balogat yoshiga yetmagansiz')
+// }else if(year > 18){
+//     message = ('siz balogat yoshiga yetgansiz')
+// }else{
+//     message = ('balogat yoshi muborak')
+// }
+// alert(message)
+
+//8) let name = prompt('Ismingizni kiriting')
+// console.log(name.split('').reverse().join(''));
+
+
+// 10)
+// for (i = 0; i < 10; i+= 2  ){
+//     console.log(i)
+// }
+
